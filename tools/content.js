cfe878d14eb4f185163a8b8e7f016ffff5c52380
Vlad <vl.tansky@gmail.com>
2020-02-21 00:54:30 +0200

    save

--- tools/content.js
+++ tools/content.js
@@ -5,11 +5,11 @@ const generateNum = 30;
 (async() => {
     await fs.remove("./src/app/tests/");
     await fs.ensureDir("./src/app/tests/");
-    for(let i=1; i<generateNum; i++){
+    for(let i=1; i<=generateNum; i++){
         await fs.copy("./src/app/source/ex", `./src/app/tests/module${i}`);
     }
     let routes = [];
-    for(let i=1;i<generateNum;i++){
+    for(let i=1;i<=generateNum;i++){
         routes.push(`{ path: 'url${i}', loadChildren: () => import('./tests/module${i}/loans2.module').then(m => m.BulkTestModule) }`)
     }
     const routesString = routes.join(",\n");
